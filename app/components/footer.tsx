5a5078b37d16c5756f02dc9b85816ab6629dcc43
nvnx7 <thenvnsahu@gmail.com>
2025-03-07 13:26:26 +0530

    refactor: text change

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -24,7 +24,7 @@ export default function Footer() {
             <ArrowIcon />
             <p className="ml-2 h-7">x</p>
           </a>
-        </li>
+        </li> */}
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
